3882c33bb40909e48c7b9a41c9a95ea2ff59b9ac
Constantin Dumitru Petre RADULICEA <constantinradulicea@gmail.com>
2024-06-24 12:52:15 +0300

    updates

--- rxtxbuffer.h
+++ rxtxbuffer.h
@@ -28,12 +28,16 @@ size_t rxtxbuffer_capacity(rxtxbuffer* pt);
 
 int rxtxbuffer_init(rxtxbuffer* _Cvector, void* buf, size_t _recved_size, size_t _sent_size, size_t _capacity);
 
+// total ammount of data that is free to be received
 size_t rxtxbuffer_rx_remaining(rxtxbuffer* sendbuf);
 
+// total ammount of tx data that remains to be sent
 size_t rxtxbuffer_tx_remaining(rxtxbuffer* sendbuf);
 
+// decrease the ammount of the tx data that remains to be sent
 void rxtxbuffer_tx_decrease_size(rxtxbuffer* pt, size_t sent_data_size);
 
+// increase the ammount of the rx data that was received
 void rxtxbuffer_rx_increase_size(rxtxbuffer* pt, size_t recved_data_size);
 
 void* rxtxbuffer_tx_buf(rxtxbuffer* pt);
